d78fc6c88333e88814c35f3cec0f41fa8c5a4686
Dacosta <dacostadesarrollo@gmail.com>
2022-03-30 22:42:40 -0500

    base estructura carpetas node

---
+++ config/mongo.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+const dbConnect = ()=>{
+    const DB_URI= process.env.DB_URI;
+    mongoose.connect(DB_URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    },(err,res)=>{
+    console.log("hola");
+
+        if (!err) {
+            console.log('*** C0NEXIÓN CORRECTA ***');
+        }else{
+            console.log("*** ERROR DE C0NEXIÓN ***");
+        }
+    });
+}
+module.exports = dbConnect;
